b2faa53142446a97716609bda0238260ba0fe0cf
Animesh Barman <barmananimesh635@gmail.com>
2025-12-02 22:59:05 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://chat-with-youtube-video.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
