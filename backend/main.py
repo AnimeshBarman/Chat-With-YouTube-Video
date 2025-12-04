4c1cd715432db6f1e639e0949bc1bbd4f75be5ce
Animesh Barman <barmananimesh635@gmail.com>
2025-12-04 13:56:35 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://chatt-tube.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
